2bfeca3a3ae1aac3d05d12c8c229d7b1cce9f4a9
Linonse <linonse@gmail.com>
2019-12-04 15:16:42 +0300

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Person(models.Model):
+    person_id = models.Index
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+# Create your models here.
